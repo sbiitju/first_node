4f1fe2f9654fac0696310fc7ab9122c18d21e121
Md. Shahin Bashar <sbiitju@gmail.com>
2023-02-20 15:10:10 +0600

    express installed successfully

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const http = require("http");
-const server = http.createServer((request,response)=>{
-        if (request.url === '/') {
-                response.write("Shahin Bashar");
-                response.end();
-        }
+// const http = require("http");
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello Shahin!')
+})
+// const server = http.createServer((request,response)=>{
+//         if (request.url === '/') {
+//                 response.write("Shahin Bashar");
+//                 response.end();
+//         }
+// });
+app.listen(3000,()=>{
+        console.log("Listening on 3000 Port");
 });
-server.listen(3000);
